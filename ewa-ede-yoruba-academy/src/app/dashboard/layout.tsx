30dc6b8749b0fd90373b0d20b582ccdd115c9a1d
obaloluwa <you@example.com>
2025-09-05 15:45:59 +0100

    New changes to fix buil errors

--- ewa-ede-yoruba-academy/src/app/dashboard/layout.tsx
+++ ewa-ede-yoruba-academy/src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <DashboardSidebar user={{} as any} />
+      <DashboardSidebar user={undefined} />
       <div className="pt-16 md:pt-0">
         <main className="min-h-[calc(100vh-4rem)]">
           <div className="px-4 py-6 sm:px-6 lg:px-8">
